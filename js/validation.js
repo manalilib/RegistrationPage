f7d1f1c56d367f6978b61f19743aac500f5d477e
Benjar Manalili <manalilibenjar@yahoo.com>
2016-10-10 14:19:30 +0800

    Major refactoring: Removed unnecessary codes and plugins.

--- js/validation.js
+++ js/validation.js
@@ -1,4 +1,3 @@
-<script type="text/javascript" src="JQuery/src/main/javascript/jquery.toastmessage.js">
 
 function validateEntries(){
 	var fName = document.register.fName;
@@ -8,36 +7,45 @@ function validateEntries(){
 	var postCode = document.register.postalCode;
 	var birthday = document.register.birthday;
 
-if(validateInput(fName) && validateInput(lName)) {
-($document).toastmessage('showErrorToast',"TRUE");  
+if(validateInput(fName) && validateInput(lName) && validateInput(street) && validateInput(town) && validateInput(postCode) && validateBDate(birthday)) {
+alert("Registration Success.");
 }
 else{
-	($document).toastmessage('showErrorToast',"FALSE");  
+alert("Registration Failed.");
 }
 
 function validateInput(inputToValidate = String){
 var pattern = /^[A-Za-z]+$/;
-var msg ="";
+var msg;
 
-if(inputToValidate == "street"){
+if(inputToValidate == "street" || inputToValidate =="fName" || inputToValidate =="lName" || inputToValidate =="town" ){
 		pattern = /^[a-zA-Z0-9]+$/; 
-		msg =" should have alphanumeric characters only" 
+		 msg ="should have alphanumeric characters only"
 	}
-else if(inputToValidate == "postCode"){
+else if(inputToValidate == "postCode" || inputToValidate =="birthday"){
 		pattern = /^[0-9]+$/;
-		msg =" should have numeric characters only"
+		 msg ="should have numeric characters only"
 	}
-
 	if(inputToValidate.value.match(pattern))  
 	{  
 	return true;  
 	}  
 	else  
 	{
-	($document).toastmessage('showErrorToast',inputToValidate + msg);  
-	inputToValidate.focus();  
+	alert(inputValidate.text + msg);
+	inputToValidate.setFocus()  
 	return false;  
 	}  
+}
+
+function validateBDate (birthday){}
+    var bday = Date.now().getUTCFullYear() - birthday.getUTCFullYear();
+if(bday >= 18){
+	return true;
+}
+esle{
+	return false;
+}
 }
 
 }
